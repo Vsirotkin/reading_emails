eb4da01c47a65f66c191e3373012f0d167dc54a0
Vik Sir <vsirotkin15@gmail.com>
2024-08-06 09:28:17 +0300

    optimization and pagination
    work.

--- myapp/services.py
+++ myapp/services.py
@@ -1,29 +1,40 @@
 import imapclient
 import pyzmail
 from django.conf import settings
-
 from myapp.models import EmailMessage
+from concurrent.futures import ThreadPoolExecutor
+from django.db import transaction
 
-def fetch_emails():
+def fetch_emails(limit=100, offset=0):
     imap_server = imapclient.IMAPClient('imap.gmail.com', ssl=True, port=993)
     imap_server.login(settings.EMAIL_USER, settings.EMAIL_PASSWORD)
     imap_server.select_folder('INBOX')
 
-    messages = imap_server.search(['ALL'])
-    for uid, message_data in imap_server.fetch(messages, ['BODY[]']).items():
+    # Получение всех UID сообщений
+    all_uids = imap_server.search(['ALL'])
+    
+    # Определение диапазона UID для пагинации
+    start_uid = all_uids[0] + offset
+    end_uid = start_uid + limit - 1
+    messages = imap_server.search(['UID', f'{start_uid}:{end_uid}'])
+
+    email_messages = []
+
+    def process_message(uid, message_data):
         email_message = pyzmail.PyzMessage.factory(message_data[b'BODY[]'])
         subject = email_message.get_subject()
         sender = email_message.get_address('from')
+        body = email_message.text_part.get_payload().decode(email_message.text_part.charset) if email_message.text_part else ""
+        email_messages.append(EmailMessage(subject=subject, sender=sender, body=body))
 
-        # Проверка наличия текстовой части
-        if email_message.text_part is not None:
-            body = email_message.text_part.get_payload().decode(email_message.text_part.charset)
-        elif email_message.html_part is not None:
-            body = email_message.html_part.get_payload().decode(email_message.html_part.charset)
-        else:
-            body = ""  # или другое значение по умолчанию
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for uid, message_data in imap_server.fetch(messages, ['BODY[]']).items():
+            executor.submit(process_message, uid, message_data)
 
-        # Сохранение сообщения в базу данных
-        EmailMessage.objects.create(subject=subject, sender=sender, body=body)
+    with transaction.atomic():
+        EmailMessage.objects.bulk_create(email_messages)
 
     imap_server.logout()
+
+# Пример вызова функции с пагинацией
+fetch_emails(limit=100, offset=0)
