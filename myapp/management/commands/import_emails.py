93bbe542f6b6c57bfa9ffb19d310303bea5a68c9
Vik Sir <vsirotkin15@gmail.com>
2024-08-07 14:24:01 +0300

    project runs.

--- myapp/management/commands/import_emails.py
+++ myapp/management/commands/import_emails.py
@@ -1,9 +1,22 @@
+import logging
 from django.core.management.base import BaseCommand
 from myapp.services import fetch_emails
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Import emails'
 
     def handle(self, *args, **options):
-        fetch_emails()
-        self.stdout.write(self.style.SUCCESS('Successfully imported emails'))
+        try:
+            emails = fetch_emails()
+            if not emails:
+                logger.info("No emails to import")
+            else:
+                for email in emails:
+                    logger.info(f"Imported email: {email.subject}")  # Используем точечную нотацию
+                self.stdout.write(self.style.SUCCESS('Successfully imported emails'))
+                logger.info('Successfully imported emails')
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error importing emails: {e}'))
+            logger.error(f'Error importing emails: {e}', exc_info=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,29 +10,29 @@ $(document).ready(function() {
 
     socket.onmessage = function(e) {
         const data = JSON.parse(e.data);
-        const emailList = document.getElementById('message-list');
+        const messageList = document.getElementById('message-list');
         const loadingItem = document.querySelector('.loading');
         if (loadingItem) {
-            emailList.removeChild(loadingItem);
+            messageList.removeChild(loadingItem);
         }
         const li = document.createElement('li');
         li.textContent = data.message;
-        emailList.appendChild(li);
+        messageList.appendChild(li);
     };
 
     socket.onclose = function(e) {
         console.error('WebSocket closed unexpectedly');
-        const emailList = document.getElementById('message-list');
+        const messageList = document.getElementById('message-list');
         const li = document.createElement('li');
         li.textContent = 'Connection closed. Please refresh the page to reconnect.';
-        emailList.appendChild(li);
+        messageList.appendChild(li);
     };
 
     socket.onerror = function(error) {
         console.error('WebSocket error:', error);
-        const emailList = document.getElementById('message-list');
+        const messageList = document.getElementById('message-list');
         const li = document.createElement('li');
         li.textContent = 'Connection error. Please try again later.';
-        emailList.appendChild(li);
+        messageList.appendChild(li);
     };
 });
